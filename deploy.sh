8e50c8621d72ca4712d8e7bbd630d8426b9ca71a
Lemonadeccc <zwjhb12@163.com>
2023-06-01 19:51:01 +0800

    fix:deploy base branch

--- deploy.sh
+++ deploy.sh
@@ -17,7 +17,7 @@ git add -A
 git commit -m 'deploy'
 
 # 如果部署到 https://<USERNAME>.github.io
-git push -f git@github.com:Lemonadeccc/seven-bit-docs.github.io.git master:gh-pages
+git push -f git@github.com:Lemonadeccc/seven-bit-docs.github.io.git master:master
 
 # 如果是部署到 https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
