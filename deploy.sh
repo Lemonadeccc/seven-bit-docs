f100f12ae139e763c7afe40150ff890a79a3dcb4
Tinker <82374719+Lemonadeccc@users.noreply.github.com>
2023-06-05 16:19:45 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -17,7 +17,7 @@ git add -A
 git commit -m 'deploy'
 
 # 如果部署到 https://<USERNAME>.github.io
-git push -f git@github.com:Lemonadeccc/seven-bit-docs.github.io.git master:dpl
+git push -f git@github.com:Lemonadeccc/seven-bit-docs.git master:dpl
 
 # 如果是部署到 https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
